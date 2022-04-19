b290584cf08429de96f5506369efedf106fde3dd
abdulrokhim <abdul.rokhim@students.itb.ac.id>
2022-04-19 12:24:05 +0700

    commit to test-branch

--- new-folder/test-2.py
+++ new-folder/test-2.py
@@ -1,5 +1,14 @@
 import math
 
 #change to be pushed
-for i in range(0,10):
-    print(math.factorial(i))
+#change utk branch
+
+def new_func():
+    for i in range(1, 11):
+        if i % 2 == 0:
+            angka = (i, math.factorial(i))
+            print(angka)
+
+new_func()
+
+
